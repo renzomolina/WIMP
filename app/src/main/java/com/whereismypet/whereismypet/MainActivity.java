feaa12d40b8174bd503456bd76abd5b7e46cae69
Gustavo David Riveros <gdrgus@gmail.com>
2018-10-13 14:22:05 -0300

    Limpiando

---
+++ app/src/main/java/com/whereismypet/whereismypet/MainActivity.java
@@ -0,0 +1,13 @@
+package com.whereismypet.whereismypet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
